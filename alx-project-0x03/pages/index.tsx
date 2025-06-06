f08c21003de9397ac2cd126e8e65a7b8f522d85f
felikz97 <felikz.kipkemoi@gmail.com>
2025-06-06 11:47:04 +0300

    3 Move your interfaces to the right directory

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,9 +1,9 @@
-import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
-import {PageRouteProps} from "@/interface";
+import Button from "@/components/common/Button";
+import { PageRouteProps } from "@/interface";
 
 interface PageRouteProps {
-  pageRoute: string
+  pageRoute: string;
 }
 
 export default function Home() {
